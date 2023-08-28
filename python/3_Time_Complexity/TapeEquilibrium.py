69de7cb9719c6e3efe867f7ee0391eada5b62272
maddenar <143417061+maddenar@users.noreply.github.com>
2023-08-28 16:36:19 -0500

    Create TapeEquilibrium.py
    100% score

---
+++ python/3_Time_Complexity/TapeEquilibrium.py
@@ -0,0 +1,71 @@
+#################################################################################################################################################################################################################################
+### TapeEquilibrium
+### Codility Test Project
+### Author: Amanda Madden
+### Modified: 8/28/2023
+### Input: non-empty integer array   Output: minimal difference 
+### Description: Set the starting current difference to infinity so when you evaluate it the first time it will be set lower.
+###              Start at the beginning of the array and add the current index value to the right sum and evaluate the lowest.
+###              Do this till the second to last element and you will be left with the lowest difference between the possible splits in the array. 
+###              
+#################################################################################################################################################################################################################################
+##A non-empty array A consisting of N integers is given. Array A represents numbers on a tape.
+##
+##Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
+##
+##The difference between the two parts is the value of: |(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
+##
+##In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
+##
+##For example, consider array A such that:
+##
+##  A[0] = 3
+##  A[1] = 1
+##  A[2] = 2
+##  A[3] = 4
+##  A[4] = 3
+##We can split this tape in four places:
+##
+##P = 1, difference = |3 − 10| = 7
+##P = 2, difference = |4 − 9| = 5
+##P = 3, difference = |6 − 7| = 1
+##P = 4, difference = |10 − 3| = 7
+##Write a function:
+##
+##def solution(A)
+##
+##that, given a non-empty array A of N integers, returns the minimal difference that can be achieved.
+##
+##For example, given:
+##
+##  A[0] = 3
+##  A[1] = 1
+##  A[2] = 2
+##  A[3] = 4
+##  A[4] = 3
+##the function should return 1, as explained above.
+##
+##Write an efficient algorithm for the following assumptions:
+##
+##N is an integer within the range [2..100,000];
+##each element of array A is an integer within the range [−1,000..1,000].
+#################################################################################################################################################################################################################################
+
+
+def solution(A):
+    array = A
+
+    totalSum = sum(array)
+    rightSum = 0
+    leftSum = 0
+    minDiff = float('inf')
+    currDiff = 0
+
+    # iterate through the array except the last element
+    for num in array[:-1]:
+        leftSum += num
+        rightSum = totalSum - leftSum
+        currDiff = abs(leftSum - rightSum)
+        minDiff = min(minDiff, currDiff)
+
+    return minDiff   
